ece69941b446caad9684472ba9f547a96124dea0
omar <>
2023-01-14 14:03:21 +0100

    initial commit: create empty project

---
+++ Guidomia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by omar on 14/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
