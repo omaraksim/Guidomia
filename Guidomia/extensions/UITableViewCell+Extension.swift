9371e7fb81fb6312f53f3f13d269eaac0351e9c6
omar <>
2023-01-14 15:19:09 +0100

    Level 1: create the mock-up screen & import essential files

---
+++ Guidomia/extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell+Extension.swift
+//  Guidomia
+//
+//  Created by omar on 14/1/2023.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+}
