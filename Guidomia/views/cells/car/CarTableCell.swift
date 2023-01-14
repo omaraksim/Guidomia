2bd79c006c9e172685f7e084a7d8462837d48c5b
omar <>
2023-01-14 23:43:16 +0100

    level 2: adding the expandable section for the cell .

--- Guidomia/views/cells/car/CarTableCell.swift
+++ Guidomia/views/cells/car/CarTableCell.swift
@@ -15,6 +15,14 @@ class CarTableCell: UITableViewCell {
     @IBOutlet weak var ratingStack: UIStackView!
     @IBOutlet weak var separatorView: UIView!
     @IBOutlet weak var separatorContainer: UIView!
+    @IBOutlet weak var expandedView: UIView!
+    @IBOutlet weak var prosLabel: UILabel!
+    @IBOutlet weak var prosStack: UIStackView!
+    @IBOutlet weak var consLabel: UILabel!
+    @IBOutlet weak var consStack: UIStackView!
+    @IBOutlet weak var consContainer: UIView!
+    @IBOutlet weak var prosContainer: UIView!
+
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +31,8 @@ class CarTableCell: UITableViewCell {
         self.nameLabel.textColor = .text
         self.priceLabel.textColor = .text
         self.separatorView.backgroundColor = .orange
+        self.prosLabel.textColor = .darkGray
+        self.consLabel.textColor = .darkGray
     }
     
     
@@ -35,6 +45,12 @@ class CarTableCell: UITableViewCell {
         self.nameLabel.text = viewModel.name
         self.priceLabel.text = viewModel.price
         self.setupRating(viewModel.rating)
+        self.setupBuckets(pros: viewModel.pros, cons: viewModel.cons)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: false)
+        self.expandedView.isHidden = !selected
     }
     
     private func setupRating(_ rating: Int){
@@ -47,6 +63,24 @@ class CarTableCell: UITableViewCell {
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.widthAnchor.constraint(equalToConstant: starWidth).isActive = true
         }
+    }
+    
+    private func setupBuckets(pros: [String], cons: [String]){
+        self.prosStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        self.prosStack.isHidden = pros.isEmpty
+        self.prosContainer.isHidden = pros.isEmpty
+        pros.forEach{ self.appendNewBullet(text: $0, stack: self.prosStack) }
+        self.consStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        self.consStack.isHidden = cons.isEmpty
+        self.consContainer.isHidden = cons.isEmpty
+        cons.forEach{ self.appendNewBullet(text: $0, stack: consStack) }
+    }
+    
+    private func appendNewBullet(text: String, stack: UIStackView){
+        let bullet = BulletView(text: text)
+        bullet.translatesAutoresizingMaskIntoConstraints = false
+        bullet.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        stack.addArrangedSubview(bullet)
     }
     
     func hideSeparator() {
